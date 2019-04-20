6d2f5a21feb8f223bb958be882806828c8b07712
Don Wasinger <DonWasinger@Dons-MacBook-Pro.local>
2019-04-20 10:23:45 -0500

    login checks if email exists before checking password

--- auth.js
+++ auth.js
@@ -2,9 +2,7 @@
 var User = require('./models/User').User;
 var md5 = require('md5');
 
-
 const checkPW = (newPW, dbPassword) => {
-  
   if (newPW === dbPassword) {
     console.log("CONGRATS! You're logged in!")
     // need to set up routing the user to the user profile page. 
@@ -36,15 +34,18 @@ module.exports = function(app){
 
   app.post('/api/login', async (req, res) => {
     console.log('PRINTED req body', req.body);
-    var reqEmail = req.body.email;                               //  set the username the user types in
-    let currentUser = await User.find({ email: reqEmail });      //  only bring back that username
-    console.log(currentUser[0]);
-
-    var dbPassword = currentUser[0].password;
-    console.log("printed from AUTH.JS", JSON.stringify(dbPassword));
-    var newPW = md5(req.body.password)              //hash what the user typed in
-    console.log('New Password: ', newPW)
-    checkPW(newPW, dbPassword);                     //call the function to check the password. 
+    var reqEmail = req.body.email;                               //  set the username the email the user types in
+    let currentUser = await User.find({ email: reqEmail });      //  only bring back that email
+    if (currentUser.length == 0) {                               // check the database for the email entered
+      console.log("That email does not exist. If you don't have an account, sign up!");
+    }else{
+      console.log('this is current user: ', currentUser[0]);
+      var dbPassword = currentUser[0].password;
+      console.log("printed from AUTH.JS", JSON.stringify(dbPassword));
+      var newPW = md5(req.body.password)              //hash what the user typed in
+      console.log('New Password: ', newPW)
+      checkPW(newPW, dbPassword);                     //call the function to check the password. 
+    }
     
   });
   
