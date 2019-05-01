bda7ac141a003f2cd7c4a14c5e045e5a055dba29
mango dyl <dylanbonds@hotmail.com>
2019-04-30 19:09:18 -0500

    banner setup

--- client/src/components/Banner/index.js
+++ client/src/components/Banner/index.js
@@ -1,13 +1,50 @@
 import React from 'react';
-import { Segment } from 'semantic-ui-react'
+import Logo from '../../images/atlas-logo-only.png';
+import { Segment, Button, Grid, Image, Icon } from 'semantic-ui-react';
+import './style.css';
 
 const bannerStyle = {
-    backgroundColor: "#556F7A"
+    backgroundColor: "#1B4965",
+    color: "#CAE9FF",
+    paddingTop: '90px',
+    width: '100%',
+    marginLeft: '0px',
+    marginRight: '0px'
 }
 
-const Banner = () => (
-    <Segment style={bannerStyle}>
+const btnStyle = {
+    margin: '20px',
+    padding: '20px 30px',
+    fontSize: '120%',
+    border: '1px black solid'
+}
 
+const Banner = pageSlide => (
+    <Segment style={bannerStyle}>
+        <Grid>
+            <Grid.Row stackable columns={3}>
+                <Grid.Column width={4}>
+                    <Image
+                        centered
+                        size="small"
+                        src={Logo}
+                    />
+                </Grid.Column>
+                <Grid.Column width={8}>
+                    <h1>Care about your security?</h1>
+                    <h2>So do we.</h2>
+                    <h3><Icon name='lock'/>Passcode protected, worry-free info storage with Atlas</h3>
+                </Grid.Column>
+                <Grid.Column width={4}>
+                    <div>
+                        <a href='#main-logo' onClick={pageSlide}><Button style={btnStyle} color='green'>Get Started</Button></a>
+                    </div>
+                    <div>
+                        <Button style={btnStyle} color='pink'>Learn How</Button>
+                    </div>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
     </Segment>
 )
 
