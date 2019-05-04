da26b859f686bb03cc010f7d9815fd242e233fba
HaydnNeese <haydnneese@yahoo.com>
2019-05-04 12:50:34 -0500

    PIN edits

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -80,19 +80,25 @@ class Home extends Component {
               note: this.state.note
             }).then(data => {
               this.handleClose()
+              this.loadModals(id);
             })
-  window.location.reload();
 }
 
-
   handleLockButtonClick = (id) => {
-
     this.setState({
       locked: false,
       selectedCardId: id
     })
   }
 
+  resetPinArrays = () => {
+    pinArray = [];
+    placeholderArray = [];
+    console.log({pinArray});
+    this.setState({
+      placeholder: ""
+    })
+  }
   createPlaceHolder = (string) => {
     this.setState({
       placeholder: string
@@ -101,10 +107,13 @@ class Home extends Component {
   }
 
   handleAnswerInput = event => {
+    //add value of click to the pinArray
     pinArray.push(event.target.value);
+    //in a separate array put a * each time a button is clicked
     placeholderArray.push('*');
+    //convert this array to a string
     let placeholderString = placeholderArray.join('');
-    console.log(placeholderString);
+    //use the function to send this string to update the state of placeholderArray
     this.createPlaceHolder(placeholderString);
   }
 
@@ -128,15 +137,27 @@ class Home extends Component {
           isCorrect: true
         });
         pinArray = [];
+        placeholderArray = [];
+        this.setState({
+          placeholder: ""
+        })
       } else if (this.state.answer === "") {
         swal("Error", "Enter your PIN to gain access", "warning");
         pinArray = [];
+        placeholderArray = [];
+        this.setState({
+          placeholder: ""
+        })
       } else {
         swal("Unable to Verify User", "", "error");
         // this.setState({
         //   attempts: this.state.attempts - 1
         // });
         pinArray = [];
+        placeholderArray = [];
+        this.setState({
+          placeholder: ""
+        })
       }
     });
     // console.log(`PIN: ${this.state.userPin}`);
@@ -228,9 +249,10 @@ class Home extends Component {
                                 handleAnswerSubmit={this.handleAnswerSubmit}
                                 attempts={this.state.attempts}
                                 placeholder={this.state.placeholder}
+                                handlePinReset={this.resetPinArrays}
                               />
                             }
-                            {
+                           {
                               this.state.selectedCardId === card._id ||
                               <LockedCard
                                 handleLockButtonClick={() => { this.handleLockButtonClick(card._id) }}
