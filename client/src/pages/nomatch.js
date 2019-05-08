4d7f7657a7d18a8eca83fff387ef24537531781d
mango dyl <dylanbonds@hotmail.com>
2019-05-07 20:41:55 -0500

    nomatch done

--- client/src/pages/nomatch.js
+++ client/src/pages/nomatch.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
 
+const nomatchStyle = {
+  marginTop: "90px",
+  textAlign: "center"
+}
+
 function NoMatch() {
   return (
-    <Container centered>
+    <Container style={nomatchStyle} centered>
       <h1>Uh oh, 404 Page Not Found!!!</h1> 
     </Container>
     
