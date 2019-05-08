2d767bd688695a163182c075d918ed37570b6a8b
mango dyl <dylanbonds@hotmail.com>
2019-05-07 20:59:58 -0500

    hash to asterisk

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-    app.get("/#", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.join(__dirname, './client/build/index.html'));
     });
 }
 else { 
     app.use(express.static(path.join(__dirname, 'client/public')));
-    app.get("/#", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.join(__dirname, './client/public/index.html'));
     });
 }
